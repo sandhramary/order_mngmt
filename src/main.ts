37433f274c0e11c381d3ceab40b9ddb97f715219
sandhra.mary@keyvalue.systems <sandhramaryprince@ll-kv-sandhra.mary.prince>
2025-06-19 14:17:31 +0530

    Switch to esm

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import "reflect-metadata"
-import express from "express"
-import AppDataSource from "./db/data-source"
+import "reflect-metadata";
+import express from "express";
+import AppDataSource from "./db/data-source.js";
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 AppDataSource.initialize()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`)
-        })
-    })
-    .catch((error) => console.log("TypeORM connection error: ", error))
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log("TypeORM connection error: ", error));
