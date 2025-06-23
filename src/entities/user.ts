e4a8744e1fb86341e4f94885512e0b9766d327e8
sandhra.mary@keyvalue.systems <sandhramaryprince@ll-kv-sandhra.mary.prince>
2025-06-23 14:06:45 +0530

    Add user entity

---
+++ src/entities/user.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column("varchar", { nullable: false })
+  name: string;
+
+  @Column("varchar", { unique: true, nullable: false })
+  emailId: string;
+
+  @Column("varchar", { nullable: false })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
